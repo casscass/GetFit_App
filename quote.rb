490b5d9bedc203fd52cc0d87162c152d34c16b0f
Lux Lu <lux.lu@android-9fc2cefd649c327c.ait-sydney.net.au>
2018-03-01 18:51:52 +1100

    blue

--- quote.rb
+++ quote.rb
@@ -2,7 +2,7 @@ module Quote
     require 'rainbow'
     module_function
     def quote 
-        quotes = [ Rainbow("Never mind the Syntx have some choc").green, Rainbow("Keep calm and code on").blue, Rainbow("something funny").yellow]
+        quotes = [ Rainbow("Never mind the Syntx have some choc").blue, Rainbow("Keep calm and code on").blue, Rainbow("something funny").yellow]
         puts quotes[rand(0..2)]
     end
 end
