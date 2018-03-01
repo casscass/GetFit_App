4282454dc146f9ce01eb7d0d58a819c73d1b312c
Lux Lu <lux.lu@alainas-iphone.ait-sydney.net.au>
2018-03-02 09:08:00 +1100

    Updated present_newtable and start_table

--- present_newtable.rb
+++ present_newtable.rb
@@ -1,6 +1,7 @@
 module StartNewTable
     require 'terminal-table'
     require_relative 'activities'
+    require 'rainbow'
     module_function
     def start_new_table
         rows = []
@@ -17,7 +18,7 @@ module StartNewTable
         rows << ['swimming', 'Wedesday', '3PM']
         rows << :separator
         rows << NewActivity.add_activities
-        table = Terminal::Table.new :title => "Updated Time Table", :headings => ['Activity', 'Date', 'Time'], :rows => rows 
+        table = Terminal::Table.new :title => Rainbow("Updated Time Table").lime, :headings => [Rainbow('Activity').greenyellow, Rainbow('Date').greenyellow, Rainbow('Time').greenyellow], :rows => rows 
         table.style = {:width => 100, :padding_left => 3, :border_x => "=", :border_i => "*"}
         puts table
     end
