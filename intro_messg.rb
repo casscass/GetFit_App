364764496d539a555b1a4a77ccd2fda2cf31324b
cassandra <cassandradaw@gmail.com>
2018-03-02 12:07:06 +1100

    removed underline from blue text

--- intro_messg.rb
+++ intro_messg.rb
@@ -1,9 +1,10 @@
 module Intro_Message
     require 'rainbow'
     require_relative 'start_table'
+   
     module_function
     def display_intro_messg
-        puts Rainbow("Below are the activities we have...Enjoy").dodgerblue.bright.underline
+        puts Rainbow("Below are the activities we have...Enjoy").dodgerblue.bright
         ExistingTable.start_existing_table
 
     end
--- welcome_messg.rb
+++ welcome_messg.rb
@@ -4,7 +4,7 @@ module Welcome_Message
     module_function
     require 'rainbow'
     def show_welcome_messg  
-        puts Rainbow("Welcome to Coder Academy Get Fit App").dodgerblue.bright.underline
+        puts Rainbow("Welcome to Coder Academy Get Fit App").dodgerblue.bright
     end
 end
 
